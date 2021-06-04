dfb6487ab4ed88d78c8c55f3a3adf3c751dff3bb
hovanhue <hovanhue2k1@gmail.com>
2021-06-04 21:33:29 +0700

    update

---
+++ module 4/ 8. AOP & Exception Handling/thuc hanh/exception-handler-customer/src/main/java/com/codegym/repository/CustomerRepository.java
@@ -0,0 +1,12 @@
+package com.codegym.repository;
+
+import com.codegym.model.Customer;
+import com.codegym.model.Province;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface CustomerRepository extends PagingAndSortingRepository<Customer, Long> {
+    Page<Customer> findAllByProvince(Province province);
+    Page<Customer> findAllByFirstNameContaining(String name, Pageable pageable);
+}
---
+++ module 4/ 8. AOP & Exception Handling/thuc hanh/exception-handler-customer/src/main/java/com/codegym/service/impl/CustomerServiecImpl.java
@@ -0,0 +1,45 @@
+package com.codegym.service.impl;
+
+import com.codegym.model.Customer;
+import com.codegym.model.Province;
+import com.codegym.repository.CustomerRepository;
+import com.codegym.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public class CustomerServiecImpl implements CustomerService {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Override
+    public Page<Customer> findAll(Pageable pageable) {
+        return customerRepository.findAll(pageable);
+    }
+
+    @Override
+    public Customer findById(Long id) {
+        return customerRepository.findOne(id);
+    }
+
+    @Override
+    public void save(Customer customer) {
+        customerRepository.save(customer);
+    }
+
+    @Override
+    public void remove(Long id) {
+        customerRepository.delete(id);
+    }
+
+    @Override
+    public Page<Customer> findAllByProvince(Province province) {
+        return customerRepository.findAllByProvince(province);
+    }
+
+    @Override
+    public Page<Customer> findAllByFirstNameContaining(String name, Pageable pageable) {
+        return customerRepository.findAllByFirstNameContaining(name, pageable);
+    }
+}
