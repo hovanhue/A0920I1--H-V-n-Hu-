94f34eb797915e42e0692345ac603180e08af7a9
hovanhue <hovanhue2k1@gmail.com>
2021-07-26 20:11:30 +0700

    up bai tap

--- module 5/thuc hanh demo/bai 3/student-management/src/app/student-list/student-list.component.ts
+++ module 5/thuc hanh demo/bai 3/student-management/src/app/student-list/student-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Istudent} from '../models/Istudent';
 
 @Component({
   selector: 'app-student-list',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentListComponent implements OnInit {
 
+  students: Istudent[] = [
+    {
+      id: 1,
+      name: 'hue',
+      age: 1,
+      mark: 1,
+      avatar: 'https://i.pinimg.com/474x/90/57/0a/90570addee2645866a597530721f37fd.jpg'
+    },
+    {
+      id: 2,
+      name: 'hue',
+      age: 1,
+      mark: 7,
+      avatar: 'https://i.pinimg.com/474x/90/57/0a/90570addee2645866a597530721f37fd.jpg'
+    },
+    {
+      id: 3,
+      name: 'hue',
+      age: 1,
+      mark: 1,
+      avatar: 'https://i.pinimg.com/474x/90/57/0a/90570addee2645866a597530721f37fd.jpg'
+    }
+];
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  displayFormDetails(id: number) {
+
+  }
 }
